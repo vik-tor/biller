2388ce3e1cdc6d3a235221d177ffb3617d185c36
Victor Maina <vmachira@gmail.com>
2024-05-03 13:36:44 +0300

    Added create function

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -34,7 +34,9 @@ class InvoiceController extends Controller
    */
   public function store(Request $request)
   {
-    //
+    $invoice = Invoice::create($request->all());
+
+    return $invoice;
   }
 
   /**
